91654f08b6683106a10f8635a2d0fa8efb0ef2d3
guledabdilatif <guledabdilatif@gmail.com>
2025-06-11 12:18:49 +0300

    auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $validatedData = $request->validate([
+            "name" => "required|string|max:255",
+            "email" => "required|email|unique:users",
+            "password" => "required|string|min:6|confirmed",
+        ]);
+        try {
+            $user = User::create([
+                'name' => $validatedData['name'],
+                'email' => $validatedData['email'],
+                'password' => Hash::make($validatedData['password']),
+            ]);
+            $token = $user->createToken('auth-token')->plainTextToken;
+
+            return response()->json([
+                "token" => $token,
+                "data" => $user,
+            ], 200);
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()]);
+        }
+    }
+    public function login(Request $request)
+    {
+        $request->validate([
+            "email" => "required|email|exists:users",
+            "password" => "required|string|min:6",
+        ]);
+
+        $credentials = ['email' => $request->email, 'password' => $request->password];
+        try {
+            if (! Auth::attempt($credentials)) {
+                return response()->json(['error' => "credentials not correct"]);
+            }
+            $user = User::where('email', $request->email)->firstOrFail();
+            $token = $user->createToken('auth-token')->plainTextToken;
+            return response()->json([
+                "token" => $token,
+                "user" => $user
+            ], 200);
+        } catch (\Exception $exception) {
+            return response()->json(['error' => $exception->getMessage()]);
+        }
+    }
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+        return response()->json(
+            [
+                'message' => "logged out successfull"
+            ], 200
+        );
+    }
+}
